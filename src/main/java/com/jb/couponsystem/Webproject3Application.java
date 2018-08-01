e59e5b24ce4d116f2bbf5eceaad6eaf250ba44f8
alzablotsky <al.zablotsky@gmail.com>
2018-08-01 10:03:36 +0300

    for deploying

---
+++ src/main/java/com/jb/couponsystem/Webproject3Application.java
@@ -0,0 +1,12 @@
+package com.jb.couponsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webproject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webproject3Application.class, args);
+	}
+}
